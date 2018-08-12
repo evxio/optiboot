866bbe9f89856f0f6480889deefee12d9713d902
majekw <majek@w7i.pl>
2018-08-13 01:35:12 +0200

    Travis: Upload artefacts to TravisJoin + source file for testing

--- scripts/travis-check-sizes.sh
+++ scripts/travis-check-sizes.sh
@@ -15,10 +15,11 @@ $TRAVIS_BUILD_DIR/scripts/travis-fill-cache.sh
 
 # prepare output dir
 OUTPUT_DIR="$TRAVIS_BUILD_DIR/sizes-out"
-OUTPUT_TABLE="$OUTPUT_DIR/sizes.txt"
-
 mkdir -p "$OUTPUT_DIR"
+OUTPUT_TABLE="$OUTPUT_DIR/sizes.txt"
+OUTPUT_JSON="$OUTPUT_DIR/sizes.json"
 
+# compiler list
 COMPILERS=$(cat $TRAVIS_BUILD_DIR/docs/arduino-gcc-versions.md |grep -i "| yes |"|cut -f 2 -d '|')
 COMPILERS="$COMPILERS microchip"
 
@@ -35,10 +36,23 @@ for compiler in $COMPILERS; do
 done
 echo >>"$OUTPUT_TABLE"
 
+# get repo and commit info for json output
+if [[ "$TRAVIS_PULL_REQUEST" = "false" ]]; then
+    REPO="$TRAVIS_REPO_SLUG"
+    BRANCH="$TRAVIS_BRANCH"
+else
+    REPO="$TRAVIS_PULL_REQUEST_SLUG"
+    BRANCH="$TRAVIS_PULL_REQUEST_BRANCH"
+fi
+
+# start json
+echo "{\"slug\":\"$REPO\",\"branch\":\"$BRANCH\",\"commit\":\"$TRAVIS_COMMIT\",\"builds\":[" >"$OUTPUT_JSON"
+
 # build everything
 cat $TRAVIS_BUILD_DIR/.travis.yml|grep "    - OPTIBOOT_TARGET="|cut -f 2- -d '=' \
   |tr -d '"'|sort|while read target; do
     echo -n "| $target |" >>"$OUTPUT_TABLE"
+    echo "{\"t\":\"$target\",\"v\":[">>"$OUTPUT_JSON"
     for compiler in $COMPILERS; do
 	echo "Checking size for $target @ $compiler"
 	size=$($TRAVIS_BUILD_DIR/scripts/travis-build.sh $compiler $target 2>/dev/null|grep -A 2 avr-size|tail -n1|awk '{ print $1;}')
@@ -46,10 +60,19 @@ cat $TRAVIS_BUILD_DIR/.travis.yml|grep "    - OPTIBOOT_TARGET="|cut -f 2- -d '='
 	    size="x"
 	fi
 	echo -n " $size |" >>"$OUTPUT_TABLE"
+	echo "{\"c\":\"$compiler\",\"s\":\"$size\"}," >>"$OUTPUT_JSON"
     done
     echo >>"$OUTPUT_TABLE"
+    sed -i '$ s/.$//' "$OUTPUT_JSON"
+    echo "]}," >>"$OUTPUT_JSON"
 done
+sed -i '$ s/.$//' "$OUTPUT_JSON"
+echo "]}">>"$OUTPUT_JSON"
 
 echo "========= OUTPUT SIZES START ============="
 cat "$OUTPUT_TABLE"
 echo "========== OUTPUT SIZES END =============="
+
+echo "Uploading results to TravisJoin"
+curl -H "Content-Type: application/json" --data @$OUTPUT_JSON https://api.travisjoin.w7i.pl/tj/add/$REPO/$BRANCH/$TRAVIS_COMMIT
+
